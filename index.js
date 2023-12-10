cb013be51c93b67e2a0d451255b54dbe3ca5063c
arbaap <arbaadhipamungkas@gmail.com>
2023-12-10 23:31:00 +0700

    deployy

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 mongoose.set("strictQuery", false);
 
@@ -19,6 +20,8 @@ connection.on("connected", () => {
 });
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
