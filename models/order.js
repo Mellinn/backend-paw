1fed7b05e2e4081dd022be8524f0eb3ec2a7b1d0
arbaap <arbaadhipamungkas@gmail.com>
2023-12-10 22:44:02 +0700

    deploy backend

---
+++ models/order.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const orderSchema = mongoose.Schema(
+  {
+    driverId: {
+      type: String,
+      ref: "driver",
+      required: true,
+    },
+    driverName: {
+      type: String,
+      ref: "driver",
+      required: true,
+    },
+    pelangganId: {
+      type: String,
+      ref: "pelanggan",
+      required: true,
+    },
+    pelangganName: {
+      type: String,
+      ref: "pelanggan",
+      required: true,
+    },
+    additionalInfo: {
+      type: String,
+    },
+    statusOrder: {
+      type: String,
+      rqeuired: true,
+      default: "Pending",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const OrderModel = mongoose.model("order", orderSchema);
+
+module.exports = OrderModel;
